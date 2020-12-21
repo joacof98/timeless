2cb7587dec868f6263debda39512060bd399130a
joaco <joacofracchia@gmail.com>
2020-12-21 12:04:09 -0300

    added pass hide

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
+// Config for token key and credentials for Cluster DB Atlas.
 module.exports = {
-    MONGOURI: 'mongodb+srv://joacof98:fuckmoash420@cluster0.zj9sv.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    MONGOURI: 'mongodb+srv://yourpasshere:fuckmoash420@cluster0.zj9sv.mongodb.net/<dbname>?retryWrites=true&w=majority',
     SECRET_KEY: 'griffithdidntdoanythingwrong'
 }
